6b0a4f8220e89ff194e4c00cb948a74e9988b432
Pratheek <pratheekharalike993@gmail.com>
2021-05-26 08:00:02 +0000

    New files

---
+++ src/main/java/com/pratheek/demo/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.pratheek.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/test-docker")
+	public String getData()
+	{
+		return "in docker test project";
+	}
+
+}
